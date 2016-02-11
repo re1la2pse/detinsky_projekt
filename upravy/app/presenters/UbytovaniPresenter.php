ded260e8a47904472b37e8607554146409ed75ac
re1la2pse <mokrusa.p@gmail.com>
2016-02-11 18:14:51 +0100

    ubytovani presenter

---
+++ upravy/app/presenters/UbytovaniPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Forms;
+use App\Model\GalerieModel;
+use Nette\Utils\Image;
+use Tracy\Debugger;
+use Tracy;
+
+class UbytovaniPresenter extends BasePresenter
+{
+
+    public function renderRezervace() {
+
+    }
+
+}
